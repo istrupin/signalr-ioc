a54822cc1ed836eeeace113623ae57a10eb4c95e
Igor Strupinskiy <IStrupinskiy@russellinvestments.com>
2018-08-10 16:33:42 -0700

    initial commit

---
+++ src/Ticker.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Ticker.Web.Startup))]
+
+namespace Ticker.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Microsoft.AspNet.SignalR.StockTicker.Startup.ConfigureSignalR(app);
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
